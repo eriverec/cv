e45afd1cabdc6056ebefe61579708d358ebd397e
eriverec <59424621+eriverec@users.noreply.github.com>
2020-06-22 19:57:49 -0500

    modal

---
+++ components/Modal.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="card">
+        <div class="card-image"></div>
+        <div class="card-content">
+          <figure class="image is-128x128 center-foto">
+            <img src="~/static/eri.jpg" alt="Placeholder image" />
+          </figure>
+
+          <div class="content">
+            <br/>
+            <h2 style="text-align: center;">Eri Cristofer Vera Iturralde</h2>
+          
+
+            <div class="field is-grouped is-grouped-multiline">
+              <div class="control">
+
+
+                <div class="box  ">
+                  <span ><font-awesome-icon icon="phone-alt" /> Telefono</span><br/>
+                  <span class="tag is-success is-medium">0 9 7 8 7 7 5 2 0 1</span>
+                </div>
+                <div class="box">
+                  <span ><font-awesome-icon icon="envelope-open" /> Email</span><br/>
+                  <span class="tag is-success is-medium">eriveraec@gmail.com</span>
+                </div>
+                <div class="box">
+                  <span ><font-awesome-icon icon="map-marker-alt" /> Direccion</span><br/>
+                  <span class="tag is-success is-medium">Cdla Los Esteros Mz. 16A1 S. 57</span>
+                </div>
+                <div class="box">
+                  <span ><font-awesome-icon icon="id-card" /> Cedula</span><br/>
+                  <span class="tag is-success is-medium">0 9 5 0 7 9 0 0 7 1</span>
+                </div>
+
+                <div class="box">
+                  <span ><font-awesome-icon icon="smile-beam" /> Estado Civil</span><br/>
+                  <span class="tag is-success is-medium">Soltero</span>
+                </div>
+
+                <div class="box">
+                  <span ><font-awesome-icon icon="birthday-cake" /> Fecha de Nacimiento</span><br/>
+                  <span class="tag is-success is-medium">14 de Marzo de 1994</span>
+                </div>
+
+                <div class="box">
+                  <span ><font-awesome-icon icon="car-side" /> Licencia de Conducir</span><br/>
+                  <span class="tag is-success is-medium">Tipo B</span>
+                </div>
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+  </div>
+</template>
+
+<style>
+.center-foto {
+  margin: auto;
+}
+.control{
+  margin: auto;
+}
+.tags{
+  justify-content: center;
+}
+.control .box{
+  width: unset !important;
+  text-align: center;
+}
+.modal body {  
+  overflow: hidden;
+  height: 100vh;
+  }
+</style>
